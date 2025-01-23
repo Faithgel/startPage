39ca00e373c3de4851a6b85d3cf411c5b37d1194
Angelo <racering14@gmail.com>
2025-01-22 21:05:04 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,14 @@ function startTime(){
     let hours = today.getHours();
     let min = today.getMinutes();
     let sec = today.getSeconds();
+    let day = today.getDay();
+    let month = today.getUTCMonth();
 
     min = checkTime(min);
 
     document.getElementById('txt').innerHTML = hours + ":" + min;
     document.getElementById('seconds').innerHTML = sec
+   
     time = setTimeout('startTime()', 500);
 }
 function checkTime(i){
@@ -15,5 +18,24 @@ function checkTime(i){
         i="0"+i;
     } return i;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const greetingElement = document.getElementById('greeting');
+    const currentHour = new Date().getHours();
+
+    let greetingMessage;
+
+    if (currentHour >= 5 && currentHour < 12) {
+        greetingMessage = 'Good Morning';
+    } else if (currentHour >= 12 && currentHour < 17) {
+        greetingMessage = 'Good Afternoon';
+    } else if (currentHour >= 17 && currentHour < 21) {
+        greetingMessage = 'Good Evening';
+    } else {
+        greetingMessage = 'Good Night';
+    }
+
+    greetingElement.textContent = greetingMessage + ", Faith";
+});
 
 startTime()
